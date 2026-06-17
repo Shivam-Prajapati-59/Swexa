fdff96642cea4e636cc0a3a224b1b664ff78d7be
Shivam Prajapati <sp488830@gmail.com>
2026-06-17 17:02:22 +0530

    refactored Backend

--- backend/src/simulation/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod account_resolver;
-pub mod fetcher;
-pub mod instruction_builder;
-pub mod simulated_quote;
-pub mod simulator;
--- backend/src/types/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod pool;
-
-pub use pool::{DEFAULT_MIN_TVL, DexProtocol, PoolEdge, PoolType, TokenMint};
